f7166e6e0ef008ad4cbd627cecc6f4052569201f
teacher019 <abdulkhalekt10@gmail.com>
2026-09-14 20:22:16 +0600

    Add django filters configuration

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -15,16 +15,16 @@ ALLOWED_HOSTS = []
 # ---------------------------------------------------------
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
-    "rest_framework",
-
-    "inventory",
+    'rest_framework',
+    'django_filters',
+    'inventory',
 ]
 
 
